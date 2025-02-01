32373b09782b7bed05690ce29799ea2eddf405ef
mark <mark@n0rt0nthec4t.dev>
2025-02-01 12:27:02 +1100

    Update checkName.ts
    Allow characters in names to match what Home app ACTUALLY supports

--- src/lib/util/checkName.ts
+++ src/lib/util/checkName.ts
@@ -9,7 +9,7 @@ import { CharacteristicValue, Nullable } from "../../types";
 export function checkName(displayName: string, name: string, value: Nullable<CharacteristicValue>): void {
 
   // Ensure the string starts and ends with a Unicode letter or number and allow any combination of letters, numbers, spaces, and apostrophes in the middle.
-  if (typeof value === "string" && !(new RegExp(/^[\p{L}\p{N}][\p{L}\p{N} ']*[\p{L}\p{N}]$/u)).test(value)) {
+  if (typeof value === "string" && !(new RegExp(/^[\p{L}\p{N}][\p{L}\p{N}\u2019 .,-]*[\p{L}\p{N}\u2019]$/u)).test(value)) {
     console.warn("HAP-NodeJS WARNING: The accessory '" + displayName + "' has an invalid '" + name + "' characteristic ('" + value + "'). Please use only " +
       "alphanumeric, space, and apostrophe characters. Ensure it starts and ends with an alphabetic or numeric character, and avoid emojis. This may prevent " +
       "the accessory from being added in the Home App or cause unresponsiveness.");
