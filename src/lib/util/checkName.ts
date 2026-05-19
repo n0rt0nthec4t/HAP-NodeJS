ba5f645008158269271c7d1ff41fe75c6534d940
mark <mark@n0rt0nthec4t.dev>
2026-05-19 10:07:53 +1000

    Fix regex to validate name format in checkName function

--- src/lib/util/checkName.ts
+++ src/lib/util/checkName.ts
@@ -10,7 +10,7 @@ export function checkName(displayName: string, name: string, value: Nullable<Cha
 
   // Ensure the string starts and ends with a Unicode letter or number.
   // Allow letters, numbers, spaces, apostrophes, and common punctuation in the middle.
-  if (typeof value === "string" && !/^[\p{L}\p{N}][\p{L}\p{N}\p{Z}\u2019'&!._:;()\/,-]*[\p{L}\p{N}]$/u.test(value)) {
+  if (typeof value === "string" && !/^[\p{L}\p{N}][\p{L}\p{N}\p{Z}\u2019'&!._:;()/,-]*[\p{L}\p{N}]$/u.test(value)) {
     console.warn("HAP-NodeJS WARNING: The accessory '" + displayName + "' has an invalid '" + name + "' characteristic ('" + value + "'). Please ensure " +
       "the name starts and ends with a letter or number. Only letters, numbers, spaces, apostrophes, and common punctuation are supported. Avoid emojis " +
       "or unsupported symbols. This may prevent the accessory from being added in the Home App or cause unresponsiveness.");
